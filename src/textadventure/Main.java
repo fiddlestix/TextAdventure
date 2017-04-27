30402da439b957670d481127e2d28a8c2951a3c2
fiddlestix <jeff.schouw@gmail.com>
2017-04-26 21:18:28 -0400

    Added a welcome message to the game, some story to rooms

--- src/textadventure/Main.java
+++ src/textadventure/Main.java
@@ -35,13 +35,24 @@ class Main {
         Scanner scanner = new Scanner(System.in);
         Command.setPlayer(player);
 
+        // Show welcome message
+        System.out.println("'Just Another Day'\n\n" +
+                           "In this game you must interact with the environment to solve puzzles and escape the house.\n" +
+                           "Manipulate objects in the world using commands such as 'take key', 'pull lever', or 'inventory'.\n" +
+                           "You can see a list of all of the commands at any time by typing 'help'.\n\n" +
+                           "Move around the map by typing directional commands such as 'north' or ('n' as a shortcut) and\n" +
+                           "'down'. Unlock doors by specifying the direction to unlock, like 'unlock north'. Note that the\n" +
+                           "key in this room does not work on this door, and you instead must use clues from the world to progress.\n\n" +
+                           "Try turning the wheel, and remember that puzzles will not always be so straightforward. There may be more than\n" +
+                           "one path to take in this strange place. Some secrets are meant to stay hidden...\n\nGood luck.\n\n");
+
         // Create game loop to wait for and capture user input
         while(true) {
 
             MapRoom currentRoom = player.getCurrentMapRoom();
 
             // Display text upon entering a room
-            System.out.println("----------------------------");
+            System.out.println("---------------------------------------------------------------------------------------------------");
             if (!currentRoom.hasRoomBeenVisited()) {
                 if (currentRoom.getRoomEntryStoryText() != null) {
                     System.out.println(FileIO.formatTextForConsole(currentRoom.getRoomEntryStoryText(), WORDWRAP_LINE_LIMITER));
